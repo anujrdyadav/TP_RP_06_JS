87b451922ff5a76964d99531e2319e733d763b62
Anuj-Persnol <anujrdyadav123@gmail.com>
2024-05-10 19:41:12 +0530

    add file

---
+++ array.js
@@ -0,0 +1,34 @@
+var people = ["Gopal","Dipu","Arjun","Aman"]
+console.log(people);
+
+var people = people[0];
+console.log(people);
+
+console.log(people.length);
+
+
+
+var netflix = ["meerjapur","Asur","Heeramdi","The Man"];
+console.log(netflix);
+
+console.log(netflix.length);
+
+netflix.push("jay")
+console.log(netflix);
+
+
+
+netflix.pop();
+console.log(netflix);
+
+
+var netflix = netflix[2];
+console.log(netflix)
+
+
+var num = [1,2,3];
+num.push(4);
+console.log(num);
+var num = num[2];
+console.log(num);
+ 
